8ad9f5913ea3a10b8eba8ffdc9aa87c4c2d5b0a8
ghit2025 <ghit@hotmail.es>
2025-06-04 13:19:59 +0200

    Implement worker registration with manager

--- DATSI/SSDD/map.2025/manager_node/manager.c
+++ DATSI/SSDD/map.2025/manager_node/manager.c
@@ -1,13 +1,84 @@
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
 #include "manager.h"
 #include "common.h"
 #include "common_srv.h"
 #include "srv_addr_arr.h"
 
+typedef struct thread_info {
+    int socket;
+    struct sockaddr_in addr;
+    srv_addr_arr *arr;
+} thread_info;
+
+static void *connection_handler(void *arg){
+    thread_info *th = arg;
+    int opcode, res;
+    unsigned short port;
+
+    if ((res=recv(th->socket, &opcode, sizeof(int), MSG_WAITALL))!=sizeof(int)){
+        if (res!=0) perror("error en recv");
+        close(th->socket);
+        free(th);
+        return NULL;
+    }
+    opcode = ntohl(opcode);
+    if ((res=recv(th->socket, &port, sizeof(unsigned short), MSG_WAITALL))!=sizeof(unsigned short)){
+        if (res!=0) perror("error en recv");
+        close(th->socket);
+        free(th);
+        return NULL;
+    }
+    if (opcode == 1) {
+        srv_addr_arr_add(th->arr, th->addr.sin_addr.s_addr, port);
+        srv_addr_arr_print("después de alta", th->arr);
+    }
+    int reply = htonl(0);
+    write(th->socket, &reply, sizeof(int));
+    close(th->socket);
+    free(th);
+    printf("conexión del cliente cerrada\n");
+    return NULL;
+}
+
 int main(int argc, char *argv[]) {
+    int s, s_conec;
+    unsigned int addr_size;
+    struct sockaddr_in clnt_addr;
+
     if (argc!=2) {
         fprintf(stderr, "Usage: %s port\n", argv[0]);
         return 1;
     }
+
+    if ((s=create_socket_srv(atoi(argv[1]), NULL)) < 0) return 1;
+
+    srv_addr_arr *arr = srv_addr_arr_create();
+    if (!arr) { perror("error creando array"); close(s); return 1; }
+
+    pthread_t thid;
+    pthread_attr_t atrib_th;
+    pthread_attr_init(&atrib_th);
+    pthread_attr_setdetachstate(&atrib_th, PTHREAD_CREATE_DETACHED);
+
+    while(1) {
+        addr_size=sizeof(clnt_addr);
+        if ((s_conec=accept(s, (struct sockaddr *)&clnt_addr, &addr_size))<0){
+            perror("error en accept");
+            close(s); return 1;
+        }
+        printf("conectado cliente con ip %s y puerto %u (formato red)\n",
+                inet_ntoa(clnt_addr.sin_addr), clnt_addr.sin_port);
+        thread_info *inf = malloc(sizeof(thread_info));
+        inf->socket = s_conec;
+        inf->addr = clnt_addr;
+        inf->arr = arr;
+        pthread_create(&thid, &atrib_th, connection_handler, inf);
+    }
+    close(s);
     return 0;
 }
--- DATSI/SSDD/map.2025/worker_node/worker.c
+++ DATSI/SSDD/map.2025/worker_node/worker.c
@@ -1,4 +1,10 @@
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/uio.h>
 #include "worker.h"
 #include "manager.h"
 #include "common.h"
@@ -10,6 +16,43 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: %s manager_host manager_port\n", argv[0]);
         return 1;
     }
+    int s, s_conec, s_mngr, res;
+    unsigned int addr_size;
+    struct sockaddr_in clnt_addr;
+    unsigned short srv_port;
+
+    if ((s=create_socket_srv(0, &srv_port)) < 0) return 1;
+    printf("puerto asignado (formato de red) %u\n", srv_port);
+
+    if ((s_mngr=create_socket_cln_by_name(argv[1], argv[2]))<0){
+        close(s); return 1;
+    }
+    int opcode = htonl(1); // identificador de alta
+    if (write(s_mngr, &opcode, sizeof(int))!=sizeof(int)){
+        perror("error en write"); close(s_mngr); close(s); return 1;
+    }
+    if (write(s_mngr, &srv_port, sizeof(unsigned short))!=sizeof(unsigned short)){
+        perror("error en write"); close(s_mngr); close(s); return 1;
+    }
+    int reply;
+    if ((res=recv(s_mngr, &reply, sizeof(int), MSG_WAITALL))!=sizeof(int)){
+        if (res!=0) perror("error en recv");
+        close(s_mngr); close(s); return 1;
+    }
+    close(s_mngr);
+
+    while(1) {
+        addr_size=sizeof(clnt_addr);
+        if ((s_conec=accept(s, (struct sockaddr *)&clnt_addr, &addr_size))<0){
+            perror("error en accept");
+            close(s); return 1;
+        }
+        printf("conectado cliente con ip %s y puerto %u (formato red)\n",
+                inet_ntoa(clnt_addr.sin_addr), clnt_addr.sin_port);
+        close(s_conec);
+        printf("conexión del cliente cerrada\n");
+    }
+    close(s);
     return 0;
 }
 
